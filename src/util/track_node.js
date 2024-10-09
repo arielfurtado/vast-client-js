0192788c02675701cdc2ba13e0cf6f26a467b73e
= <lucio.furtado@g.globo>
2024-10-09 08:43:26 -0300

    refactor: Update track_node.js to use XMLHttpRequest for making HTTP requests

--- src/util/track_node.js
+++ src/util/track_node.js
@@ -1,16 +1,20 @@
-const http = require('http');
-const https = require('https');
-const url = require('url');
-
 import { util } from './util';
 
 export default function track(URLTemplates, macros, options) {
   const URLs = util.resolveURLTemplates(URLTemplates, macros, options);
 
   URLs.forEach((URL) => {
-    const parsedURL = url.parse(URL);
-    const protocol = parsedURL.protocol;
-    const httpModule = protocol === 'https:' ? https : http;
-    httpModule.get(URL);
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', URL, true);
+    xhr.onload = function () {
+      if (!(xhr.status >= 200 && xhr.status < 300)) {
+        console.warn('Ad Track Request failed with status', xhr.status);
+      }
+    };
+    xhr.onerror = function () {
+      console.warn('Ad Track Request error', xhr.status, xhr.statusText);
+    };
+    xhr.send();
   });
 }
